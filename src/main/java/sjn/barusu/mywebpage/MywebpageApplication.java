e606ef1068516e28e811209fce71219f942d18c6
sejinsjn <sedad_@hotmail.de>
2022-10-19 17:25:52 +0200

    Initial commit

---
+++ src/main/java/sjn/barusu/mywebpage/MywebpageApplication.java
@@ -0,0 +1,13 @@
+package sjn.barusu.mywebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywebpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sjn/barusu/mywebpage/MywebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package sjn.barusu.mywebpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
