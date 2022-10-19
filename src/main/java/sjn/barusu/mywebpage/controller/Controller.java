b1809ff1e65a322ca1e0a47aad8ccc52b360f87c
sejinsjn <sedad_@hotmail.de>
2022-10-19 18:13:29 +0200

    Hello test 2

---
+++ src/main/java/sjn/barusu/mywebpage/controller/Controller.java
@@ -0,0 +1,13 @@
+package sjn.barusu.mywebpage.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from here";
+    }
+}
