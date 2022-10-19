3151e4f634e3826ffbbf15bb08319faf8a53c0be
sejinsjn <sedad_@hotmail.de>
2022-10-19 18:11:47 +0200

    Hello test 2

--- src/main/java/sjn/barusu/mywebpage/MywebpageApplication.java
+++ src/main/java/sjn/barusu/mywebpage/MywebpageApplication.java
@@ -3,11 +3,12 @@ package sjn.barusu.mywebpage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class MywebpageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MywebpageApplication.class, args);
 	}
-
 }
