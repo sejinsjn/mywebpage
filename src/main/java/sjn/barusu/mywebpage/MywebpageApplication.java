ab3038fb6af8a9fde8d69d1948d7c30526df7873
sejinsjn <sedad_@hotmail.de>
2022-10-20 21:16:29 +0200

    Hi working

--- src/main/java/sjn/barusu/mywebpage/MywebpageApplication.java
+++ src/main/java/sjn/barusu/mywebpage/MywebpageApplication.java
@@ -2,11 +2,16 @@ package sjn.barusu.mywebpage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sjn.barusu.mywebpage.sheets.sheets;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
 
 @SpringBootApplication
 public class MywebpageApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws GeneralSecurityException, IOException {
 		SpringApplication.run(MywebpageApplication.class, args);
 	}
 }
--- src/main/java/sjn/barusu/mywebpage/controller/MyController.java
+++ src/main/java/sjn/barusu/mywebpage/controller/MyController.java
@@ -3,12 +3,17 @@ package sjn.barusu.mywebpage.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import sjn.barusu.mywebpage.sheets.sheets;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 @Controller
 public class MyController {
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model) throws GeneralSecurityException, IOException {
+        model.addAttribute("gen8", sheets.getData("Gen 8!A1:R23"));
         return "index";
     }
 }
